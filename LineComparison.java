ac49746c4e6bcf7fca9d4480f1f173eaeda698e0
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-09-26 00:33:38 +0530

    Added: Check equality of the length of two lines using java equals method added

--- LineComparison.java
+++ LineComparison.java
@@ -5,24 +5,35 @@ public class LineComparison {
 	public static void main(String[] args) {
 		// variables
 		// pointOne random x-y coordinates between 0 to 10
-		double x1 = Math.floor(Math.random() * 10) + 1;
-		double y1 = Math.floor(Math.random() * 10) + 1;
+		double x1LineOne = Math.floor(Math.random() * 10) + 1;
+		double y1LineOne = Math.floor(Math.random() * 10) + 1;
+		double x1LineTwo = Math.floor(Math.random() * 10) + 1;
+		double y1LineTwo = Math.floor(Math.random() * 10) + 1;
 		// pointTwo random x-y coordinates between 0 to 10
-		double x2 = Math.floor(Math.random() * 10) + 1;
-		double y2 = Math.floor(Math.random() * 10) + 1;
-		// length of line
-		int length = 0;
+		double x2LineOne = Math.floor(Math.random() * 10) + 1;
+		double y2LineOne = Math.floor(Math.random() * 10) + 1;
+		double x2LineTwo = Math.floor(Math.random() * 10) + 1;
+		double y2LineTwo = Math.floor(Math.random() * 10) + 1;
 
 		// welcome message
 		System.out.println("Welcome to Line Comparison Computation Program!");
+		System.out.println("lengthLineOne\tlengthLineTwo");
+		System.out.println("---------------------------");
 
-		System.out.println("x1\ty1\tx2\ty2");
-		System.out.println("---------------------------");
-		System.out.println(x1 + "\t" + y1 + "\t" + x2 + "\t" + y2);
-		System.out.println("---------------------------");
 		// calculation of length of the line from pointOne to pointTwo
-		length = (int) Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-		System.out.println("Length of the line is " + length + " units");
+		Integer lengthLineOne = (int) Math
+				.sqrt(Math.pow(x2LineOne - x1LineOne, 2) + Math.pow(y2LineOne - y1LineOne, 2));
+		Integer lengthLineTwo = (int) Math
+				.sqrt(Math.pow(x2LineTwo - x1LineTwo, 2) + Math.pow(y2LineTwo - y1LineTwo, 2));
+		System.out.println(lengthLineOne + " units\t\t" + lengthLineTwo + " units");
+		System.out.println("---------------------------");
+
+		// Checking the equality of the length of two lines using Java equals method
+		boolean areEqual = lengthLineOne.equals(lengthLineTwo);
+		if (areEqual)
+			System.out.println("The length of the two lines are equal");
+		else
+			System.out.println("The length of the two lines are not equal");
 	}
 
 }
