5e9b469811232f25eb088613acffb0aedd4db85a
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-09-25 19:23:42 +0530

    Added: Welcome message added

---
+++ LineComparison.java
@@ -0,0 +1,11 @@
+package lineComparison;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		// Welcome message
+		System.out.println("Welcome to Line Comparison Computation Program!");
+
+	}
+
+}
