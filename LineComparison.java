6bcb371a6df3ec078f3e127fee63936e5e5fab81
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-09-26 11:11:16 +0530

    Refactor: Code refactored to add classes and methods

--- LineComparison.java
+++ LineComparison.java
@@ -3,33 +3,41 @@ package lineComparison;
 public class LineComparison {
 
 	public static void main(String[] args) {
-		// variables
-		// pointOne random x-y coordinates between 0 to 10
-		double x1LineOne = Math.floor(Math.random() * 10) + 1;
-		double y1LineOne = Math.floor(Math.random() * 10) + 1;
-		double x1LineTwo = Math.floor(Math.random() * 10) + 1;
-		double y1LineTwo = Math.floor(Math.random() * 10) + 1;
-		// pointTwo random x-y coordinates between 0 to 10
-		double x2LineOne = Math.floor(Math.random() * 10) + 1;
-		double y2LineOne = Math.floor(Math.random() * 10) + 1;
-		double x2LineTwo = Math.floor(Math.random() * 10) + 1;
-		double y2LineTwo = Math.floor(Math.random() * 10) + 1;
 
 		// welcome message
 		System.out.println("Welcome to Line Comparison Computation Program!");
-		System.out.println("lengthLineOne\tlengthLineTwo");
-		System.out.println("---------------------------");
 
-		// calculation of length of the line from pointOne to pointTwo
-		Integer lengthLineOne = (int) Math
-				.sqrt(Math.pow(x2LineOne - x1LineOne, 2) + Math.pow(y2LineOne - y1LineOne, 2));
-		Integer lengthLineTwo = (int) Math
-				.sqrt(Math.pow(x2LineTwo - x1LineTwo, 2) + Math.pow(y2LineTwo - y1LineTwo, 2));
+		// creates LineComparison object
+		LineComparison lineComparison = new LineComparison();
+
+		// creates Point objects
+		Point pointOne = new Point();
+		Point pointTwo = new Point();
+		Point pointThree = new Point();
+		Point pointFour = new Point();
+
+		// creates Line objects
+		Line lineOne = new Line(pointOne, pointTwo);
+		Line lineTwo = new Line(pointThree, pointFour);
+
+		// invoking calculateLength method of Line class
+		Integer lengthLineOne = lineOne.calculateLineLength();
+		Integer lengthLineTwo = lineTwo.calculateLineLength();
+
+		System.out.println("lengthLineOne\tlengthLineTwo");
+		System.out.println("------------------------------");
 		System.out.println(lengthLineOne + " units\t\t" + lengthLineTwo + " units");
-		System.out.println("---------------------------");
+		System.out.println("------------------------------");
+
+		// Checking the equality of the length of two lines using equals method
+		boolean areEqual = lineComparison.equals(lengthLineOne, lengthLineTwo);
+		if (areEqual)
+			System.out.println("The length of the two lines are equal");
+		else
+			System.out.println("The length of the two lines are not equal");
 
 		// Checking the equality of the length of two lines using compareTo method
-		int value = lengthLineOne.compareTo(lengthLineTwo);
+		int value = lineComparison.compareTo(lengthLineOne, lengthLineTwo);
 		if (value < 0)
 			System.out.println("The length of line one is less than the length of line two");
 		else if (value == 0)
@@ -38,4 +46,83 @@ public class LineComparison {
 			System.out.println("The length of line one is greater than the length of line two");
 	}
 
+	public boolean equals(Integer lengthLineOne, Integer lengthLineTwo) {
+		System.out.println("Equals method used!");
+		return lengthLineOne.equals(lengthLineTwo);
+	}
+
+	public int compareTo(Integer lengthLineOne, Integer lengthLineTwo) {
+		System.out.println("CompareTo method used!");
+		return lengthLineOne.compareTo(lengthLineTwo);
+	}
+}
+
+class Point {
+	private double xCoordinate;
+	private double yCoordinate;
+
+	// constructor
+	public Point() {
+		this.xCoordinate = Math.floor(Math.random() * 10) + 1;
+		this.yCoordinate = Math.floor(Math.random() * 10) + 1;
+	}
+
+	// getters and setters
+	public double getxCoordinate() {
+		return xCoordinate;
+	}
+
+	public void setxCoordinate(double xCoordinate) {
+		this.xCoordinate = xCoordinate;
+	}
+
+	public double getyCoordinate() {
+		return yCoordinate;
+	}
+
+	public void setyCoordinate(double yCoordinate) {
+		this.yCoordinate = yCoordinate;
+	}
+}
+
+class Line {
+	private Point pointOne;
+	private Point pointTwo;
+
+	// constructor
+	public Line(Point one, Point two) {
+		this.pointOne = one;
+		this.pointTwo = two;
+	}
+
+	// getters and setters
+	public Point getPointOne() {
+		return pointOne;
+	}
+
+	public void setPointOne(Point pointOne) {
+		this.pointOne = pointOne;
+	}
+
+	public Point getPointTwo() {
+		return pointTwo;
+	}
+
+	public void setPointTwo(Point pointTwo) {
+		this.pointTwo = pointTwo;
+	}
+
+	public int calculateLineLength() {
+		// storing pointOne coordinates in x1 and y1
+		double x1 = pointOne.getxCoordinate();
+		double y1 = pointOne.getyCoordinate();
+
+		// storing pointTwo coordinates in x2 and y2
+		double x2 = pointTwo.getxCoordinate();
+		double y2 = pointTwo.getyCoordinate();
+
+		// computing length
+		int length = (int) Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		return length;
+	}
 }
